74b5d4c1e212a1c9f46c8751c849c5be327b41ee
st10036905 <st10036905@vcconnect.edu.za>
2024-09-25 23:33:37 +0200

    Adding code to the backend.

--- app/src/main/java/com/example/medcheck/Login.kt
+++ app/src/main/java/com/example/medcheck/Login.kt
@@ -5,17 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.medcheck.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Initialize ViewBinding
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        // Set the content view to the root view of the binding
+        setContentView(binding.root)
+
+
+
+
 
     }
 }
