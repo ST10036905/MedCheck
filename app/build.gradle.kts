064dd2d7b758cff9be6f084aabac0a0124bdcf70
st10068763 <anterozacarias007@gmail.com>
2024-09-25 21:03:11 +0200

    Changed the map style and added more map functions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.google.maps)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.places)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/medcheck/GoogleMap.kt
+++ app/src/main/java/com/example/medcheck/GoogleMap.kt
@@ -11,17 +11,23 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.libraries.places.api.net.PlacesClient
 
 class GoogleMap : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var radioGroup: RadioGroup
-    // declacring the map variable
+    // declaring the map variable
     private var map : GoogleMap? = null
+    // declaring the search
+    private lateinit var placesClient: PlacesClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_google_map)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -50,14 +56,24 @@ class GoogleMap : AppCompatActivity(), OnMapReadyCallback {
         mapFragment?.getMapAsync(this)
     }
     override fun onMapReady(map: GoogleMap?) {
+        // setting a style for the map
+        map?.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style))
         // function to set the map type
         this.map = map
         map?.mapType = GoogleMap.MAP_TYPE_NORMAL
-        // adding zoom function
-        map?.uiSettings?.isZoomControlsEnabled = true
-        map?.uiSettings?.isZoomGesturesEnabled = true
+
         // sets the map default location
        val latLng = LatLng(-33.964314, 18.467859)
-        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,19f))
+        map?.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15f))
+
+        // adding map functions
+        map?.uiSettings?.isZoomControlsEnabled = true
+        map?.uiSettings?.isZoomGesturesEnabled = true
+        map?.uiSettings?.isCompassEnabled = true
+        map?.uiSettings?.isRotateGesturesEnabled = true
+        map?.uiSettings?.isTiltGesturesEnabled = true
+        map?.uiSettings?.isScrollGesturesEnabled = true
+        map?.uiSettings?.isScrollGesturesEnabledDuringRotateOrZoom = true
+        map?.uiSettings?.isMyLocationButtonEnabled = true
     }
 }
