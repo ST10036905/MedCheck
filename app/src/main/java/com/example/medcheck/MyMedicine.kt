cfba529a4482d5b7b779470c402370daec058860
Azania Aria Ncube <ST10036066@vcconnect.edu.za>
2024-09-26 18:05:14 +0200

    Setting an OnClickListener on the button(stop medication) shown on Medication details screen at the button, when the user clicks on it, it sends them to MyMedicine.kt aka My Medicine screen to delete or add new medicine

--- app/src/main/java/com/example/medcheck/MyMedicine.kt
+++ app/src/main/java/com/example/medcheck/MyMedicine.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MyMedicineActivity : AppCompatActivity() {
+class MyMedicine : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
