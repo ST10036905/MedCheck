754bfb6e8eccafa9db7b7350e9c616ee5a7678f2
st10068763 <anterozacarias007@gmail.com>
2024-10-27 16:06:03 +0200

    Fixed the scroll view in the settings, added a language option with a fragment for the user to select the app language

--- app/src/main/java/com/example/medcheck/CustomInfoWindowAdapter.kt
+++ app/src/main/java/com/example/medcheck/CustomInfoWindowAdapter.kt
@@ -9,21 +9,20 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.Marker
 
 class CustomInfoWindowAdapter (private val context : Context) : GoogleMap.InfoWindowAdapter{
-    override fun getInfoContents(p0: Marker): View? {
-        return null
-    }
-
-    override fun getInfoWindow(marker: Marker): View? {
+    override fun getInfoContents(marker: Marker): View? {
         val view = LayoutInflater.from(context).inflate(R.layout.custom_info_window, null)
         val title : TextView = view.findViewById(R.id.infoWindowTitle)
         val desc : TextView = view.findViewById(R.id.infoWindowContent)
         val image : ImageView = view.findViewById(R.id.infoWindow)
-
 
         val data = marker.tag as CustomInfoWindowData
         title.text = data.title
         desc.text = data.desc
         data.image.let { image.setImageResource(it) }
         return view
+    }
+
+    override fun getInfoWindow(marker: Marker): View? {
+       return null
     }
 }
