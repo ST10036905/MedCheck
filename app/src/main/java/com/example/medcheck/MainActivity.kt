57f4a94a6292569faf553e75b89cd0a3099979bd
st10068763 <anterozacarias007@gmail.com>
2024-11-04 14:24:32 +0200

    Fixing issues

--- app/src/main/java/com/example/medcheck/MainActivity.kt
+++ app/src/main/java/com/example/medcheck/MainActivity.kt
@@ -4,10 +4,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Intent
 import android.content.SharedPreferences
-
 import android.Manifest
-import android.content.Intent
-import android.content.SharedPreferences
 import android.content.pm.PackageManager
 
 import android.os.Build
